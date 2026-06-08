d7a19f31fd595bbbe5b08efb6b2492d989ea820e
aadi-codeSs <adityasingh0999@outlook.com>
2026-06-08 14:14:31 +0530

    feat: add redirect from /main to /login in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/main",
+        destination: "/login",
+        permanent: false,
+      },
+    ];
+  },
+
   images: {
     remotePatterns: [
       {
